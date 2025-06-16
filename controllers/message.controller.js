9bd0f1f67dd0d722204e70e9be4c6d46b1f3bf2e
jeevan42 <g1kumar05092002@gmail.com>
2025-06-16 18:48:57 +0530

    added routes and controllers

---
+++ controllers/message.controller.js
@@ -0,0 +1,10 @@
+import Message from "../models/message.model.js";
+
+export const getMessages = async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.json(messages);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
